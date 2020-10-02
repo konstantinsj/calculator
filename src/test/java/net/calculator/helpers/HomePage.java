d66135c371e31b7f3f0fc0ff2e27580353dfecf6
konstantinsj <60075680+konstantinsj@users.noreply.github.com>
2020-10-02 03:14:26 +0300

    working

--- src/test/java/net/calculator/helpers/HomePage.java
+++ src/test/java/net/calculator/helpers/HomePage.java
@@ -16,99 +16,27 @@ import static junit.framework.TestCase.assertEquals;
 public class HomePage extends TestBase {
 
     //locators
-    private By registrationEmail = By.name("email");
-    private By registrationPassword = By.name("password");
-    private By press1 = By.cssSelector("span.scinm:nth-child(1)");
-    private By registrationCheckboxRules = By.xpath("//body//form//span[2]");
-    private By result = By.id("sciOutPut");
-    private By input = By.cssSelector("#sciInPut");
-    private By pressKey = By.xpath("//span[@onclick='r(3)']");
-
     private String pageUrl = "https://www.calculator.net";
-    private String registrationPage = "/signup/";
-    private By historyProductsLocator = By.cssSelector(".product.js-product");
-    private By topCartLocator = By.id("top_cart_counter");
-    private By productGridSelector = By.cssSelector(".col.col--xs-4.product");
-    private By totalProductsPriceLocator = By.id("total_products_price_without_cupon");
-    private By addToCartButtonLocator = By.cssSelector(".btn--280");
-    private By removeTopItemFromCartLocator = By.cssSelector(".btn.btn--square.btn--simple-error.btn--smaller");
-    private By topItemInCartPriceLocator = By.xpath("//li[1]/div/div[1]/p/b");
-    private By productPriceLocator = By.xpath("//strong");
+    private By result = By.id("sciOutPut");
+    private By history = By.id("scihistory");
 
     public void homePage() {
         driver.get(pageUrl);
     }
 
-    private void historyPage() {
-        driver.get(pageUrl + "/recent_history/");
-    }
-
-    private void cartPage() {
-        driver.get(pageUrl + "/cart/lv");
-    }
-
-    private void addProductToCart() {
-        js.executeScript("window.scrollBy(0,200)", "");
-        driver.findElement(addToCartButtonLocator).click();
-    }
-
     public String getResult() {
-    //    return Double.parseDouble((driver.findElement(By.id("sciOutPut")).getAttribute("innerText").replace('\u00A0',' ').trim()));
-        return (driver.findElement(By.id("sciOutPut")).getAttribute("innerText").replace('\u00A0',' ').trim());
+        return driver.findElement(result).getAttribute("innerText").replace('\u00A0',' ').trim();
+    }
+
+    public String getHistory() {
+        return driver.findElement(history).getAttribute("innerText").replace('\u00A0',' ').trim();
     }
 
     public void input(String inputData) {
         new Actions(driver).sendKeys(inputData).perform();
-        //driver.findElement(input).sendKeys(inputData);
-        //WebElement element =driver.findElement(input);
-        //((JavascriptExecutor)driver).executeScript("var ele=arguments[0]; ele.innerText = '"+inputData +"';", element);
-    }
-
-
-
-    public double getTotalPriceFromCart() {
-        cartPage();
-        WebElement element = driver.findElement(totalProductsPriceLocator);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        return Double.parseDouble(driver.findElement(totalProductsPriceLocator).getAttribute("innerText"));
     }
 
     public void pressKey(String key) {
         driver.findElement(By.xpath("//span[@onclick=\"r(" +key +")\"]")).click();
-    }
-    public void selectAvailableRandomProduct() {
-        //clicking on random product from N pages and selecting N product on that page
-
-        int randomPage = ThreadLocalRandom.current().nextInt(1, 500);
-        int randomProduct = ThreadLocalRandom.current().nextInt(0, 10);
-
-     //   driver.get(pageUrl + availableProducts + randomPage + "/");
-        wait.until(ExpectedConditions.elementToBeClickable(productGridSelector));
-        List<WebElement> productGrid = driver.findElements(productGridSelector);
-        productGrid.get(randomProduct).click();
-    }
-
-    public String[] openRandomProducts(int itemQuantity) {
-        String[] clickedItemArray = new String[itemQuantity];  //
-
-        for (int i = 0; i < itemQuantity; i++) {
-            selectAvailableRandomProduct();
-            clickedItemArray[i] = driver.getCurrentUrl();
-        }
-        return clickedItemArray;
-    }
-
-
-    public double removeTopItemFromCart(int quantity) {
-
-        double removedItemPrice = 0.00;
-
-        for (int i = 0; i < quantity; i++) {
-            cartPage();
-            removedItemPrice = removedItemPrice + Double.parseDouble(driver.findElement(topItemInCartPriceLocator).getAttribute("innerText"));
-            WebElement removeTopItemFromCart = driver.findElements(removeTopItemFromCartLocator).get(0);
-            js.executeScript("arguments[0].click();", removeTopItemFromCart);
-        }
-        return removedItemPrice;
     }
 }
--- src/test/java/net/calculator/tests/CalculatorTest.java
+++ src/test/java/net/calculator/tests/CalculatorTest.java
@@ -12,34 +12,41 @@ public class CalculatorTest extends TestBase {
     HomePage home = new HomePage();
 
     @Test
-    public void testBasicTest() {
+    public void testDecimal() {
 
-        String x= String.valueOf(3);
-
-        home.homePage();
-        home.pressKey(x);
-
-        Assert.assertEquals("Expected",x,home.getResult());
-    }
-
-    @Test
-    public void testDivide() {
-
-        // we using button to input here
+        // we using buttons to input and checking long decimals
         home.homePage();
         home.pressKey("1");
         home.pressKey("'/'");
         home.pressKey("3");
 
-        Assert.assertEquals("Expected",(truncateExpected(1.0/3.0)),Double.parseDouble(home.getResult()), 0.0000000001);
+        Assert.assertEquals((truncateExpected(1.0/3.0)),Double.parseDouble(home.getResult()), 0.0000000001);
     }
 
     @Test
     public void testLongValues() {
 
+        // we using keyboard input and checking long value
         home.homePage();
         home.input("2222222222222 * 33333333 =");
         Assert.assertEquals( "74074073333325920000",home.getResult());
+    }
+
+    @Test
+    public void testNegativeAndHistory() {
+
+        //multiplication of negative number
+        home.homePage();
+        home.input("-5 * 3 =");
+        Assert.assertEquals( -15,Integer.parseInt(home.getResult()));
+
+        //subtraction of negative number
+        home.pressKey("'C'");
+        home.input("3 - 5");
+        Assert.assertEquals( -2,Integer.parseInt(home.getResult()));
+
+        //checking history
+        Assert.assertEquals("-5 × 3 = -15", home.getHistory());
     }
 
     @Test
@@ -55,11 +62,13 @@ public class CalculatorTest extends TestBase {
         home.input(randomNum1 + "/" +randomNum2 + "+" +randomNum3 + "*" +randomNum4 +"-" +randomNum5);
 
         double ExpectedCalc = (randomNum1/randomNum2+randomNum3*randomNum4-randomNum5);
-        Assert.assertEquals(truncateExpected(ExpectedCalc), Double.parseDouble(home.getResult()), 0.000001);
+        Assert.assertEquals(truncateExpected(ExpectedCalc), Double.parseDouble(home.getResult()), 0.001);
     }
+
     @Test
     public void testBracketsAndBackButton() {
 
+        //testing that operation in brackets performs before multiplication
         home.homePage();
         home.input("(2 + 2) × 2");
         Assert.assertEquals(8, Integer.parseInt(home.getResult()));
@@ -68,13 +77,15 @@ public class CalculatorTest extends TestBase {
         home.pressKey("'bk'");home.pressKey("'bk'");
         Assert.assertEquals(4, Integer.parseInt(home.getResult()));
     }
+
     @Test
     public void testRandomDivideBy0() {
 
+        // generating random number using calculator and diving in to 0
         home.homePage();
         home.pressKey("'RND'");
 
-        //checking that random number don't give any error
+        //checking that random number don't give any error by itself before diving
         Assert.assertNotEquals("Error", home.getResult());
 
         home.pressKey("'/'");
@@ -85,6 +96,7 @@ public class CalculatorTest extends TestBase {
     @Test
     public void testMultiplicationTable() {
 
+        // checking 10x10 multiplication table
         home.homePage();
         for (int i =1; i <= 10; i++){
             //cycles through the first number to multiply
